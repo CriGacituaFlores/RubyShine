b81c6b7c25888fdfcff1ca764384a4b4c6e1e767
Cristian <cgacitua@webdox.cl>
2017-03-08 12:03:32 -0300

    Add relation for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
 	belongs_to :state
+	has_many :customers_billing_addresses
+
+	after_touch :log_when_address_touched
+
+	private
+	def log_when_address_touched
+		"Address was touched!"
+	end
 end
