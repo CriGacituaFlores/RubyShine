208154d389fbcc9a0af213f9f4489fa41fe89a86
Cristian <cgacitua@webdox.cl>
2017-03-29 16:12:16 -0300

    Añadiendo parametros fuertes a la vista materializada

--- app/models/customer_detail.rb
+++ app/models/customer_detail.rb
@@ -1,4 +1,26 @@
 class CustomerDetail < ActiveRecord::Base
 	#SE USA SELF PRIMARY KEY YA QUE LA VISTA NO TIENE UN CAMPO LLAMADO 'ID'
 	self.primary_key = 'customer_id'
+
+	def update(params)
+		Customer.find(self.customer_id).update(
+			params.permit(:first_name,:last_name,:username,:email))
+
+		Address.find(self.billing_address_id).update(
+			address_attributes(params,"billing"))
+
+		Address.find(self.shipping_address_id).update(
+			address_attributes(params,"shipping"))
+	end
+
+	private
+
+	def address_attributes(params, type)
+		{
+			street: params["#{type}_street"],
+			city: params["#{type}_city"],
+			state: State.find_by_code(params["#{type}_state"]),
+			zipcode: params["#{type}_zipcode"]
+		}
+	end
 end
